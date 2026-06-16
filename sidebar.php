f39043969fb353d0e82f1cf69fb29b03a58b3024
Hud0304 <nurulhuda.0304@gmail.com>
2026-06-16 09:51:18 +0800

    Fix module1 sidebar issue

--- sidebar.php
+++ sidebar.php
@@ -2,14 +2,16 @@
     <nav class="nav-menu">
         <ul>
             <?php if(isset($_SESSION['user_type'])): ?>
-                <li><a href="../module1/admin_dashboard.php">Dashboard</a></li>
+                <li><a href="admin_dashboard.php">Dashboard </a></li>
                 <?php if($_SESSION['user_type'] == 'Admin'): ?>
-                    <li><a href="../module1/user_management.php">Users</a></li>
+                    <li><a href="user_management.php">Users</a></li>
                 <?php endif; ?>
                 <li><a href="club_list.php">Clubs</a></li>
+                <?php if($_SESSION['user_type'] !== 'Admin'): ?>
                 <li><a href="event_management.php">Events</a></li>
+                <?php endif; ?>
                 <li><a href="attendance.php">Attendance</a></li>
-                <li><a href="participant_reports.php">Reports</a></li>
+                <li><a href="participan_reports.php">Reports</a></li>
                 <li><a href="profile.php">Profile</a></li>
                 <li><a href="logout.php">Logout</a></li>
             <?php else: ?>
